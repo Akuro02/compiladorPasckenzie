2d7fa7a1de584afb5da3adbadf6994c3ff871627
Marco Camargo <marcocamargo04@gmail.com>
2025-09-23 10:33:30 -0300

    feito em sala

--- pasckenzie.c
+++ pasckenzie.c
@@ -3,6 +3,20 @@
 typedef enum{
     ERRO,
     IDENTIFIER,
+    KW_VAR,
+    KW_PERIOD,
+    KW_COMMA,
+    KW_DOUBLEDOT,
+    KW_SEMICOLON,
+    KW_OPENPARENTHESIS,
+    KW_CLOSEPARENTHESIS,
+    KW_READ,
+    KW_WRITE,
+    KW_IF, KW_THEN, KW_ELSE,
+    KW_WHILE, KW_DO,
+    RELATIONAL_OPERATOR,
+    ADDING_OPERATOR,
+    MULTIPLYING_OPERATOR,
     TYPE,
     FACTOR,
     EOS
@@ -37,6 +51,9 @@ int main(){
     nLinha = 1;
 
     infoAtomo = obterAtomo();
+    lookAhead = infoAtomo.atomo;
+
+    program();
 }
 
 
@@ -75,16 +92,62 @@ TInfoAtomo obterAtomo(){
     }
 
     infoAtomo.linha = nLinha;
-
-    if(*buffer == '\0'){
-        infoAtomo.atomo = EOS;
-        return infoAtomo;
-    }
-
     // Fazer as comparações
 
     printf("Linha %i\n", nLinha);
+    if(islower(*buffer)){
+        reconheceID(&infoAtomo);
+    }
+    else if(*buffer == ';'){
+        buffer++;
+        infoAtomo.atomo = KW_SEMICOLON;
+    }
+    else if(*buffer == '.'){
+        buffer++;
+        infoAtomo.atomo = KW_PERIOD;
+    }
+    else if(*buffer == '\0'){
+        infoAtomo.atomo = EOS;
+    }
     return infoAtomo;
 
 
+}
+
+TInfoAtomo reconheceID(TInfoAtomo* infoAtomo){
+    char* ini_lexema = buffer;
+
+q1:
+    if( islower(*buffer) || isdigit(*buffer) ){
+        buffer++;
+        goto q1;
+    }
+    if( isupper(*buffer)) 
+        return; // sai com erro
+    
+    // preenche o atributo do atomo IDENTIFICADOR
+    strncpy(infoAtomo->atributo.id,ini_lexema,buffer-ini_lexema);
+    infoAtomo->atributo.id[buffer-ini_lexema] = '\0'; // aqui temos um IDENTIFICADOR
+
+    infoAtomo->atomo = IDENTIFIER;
+
+    return ;
+}
+
+void consome( TAtomo atomo ){
+    if( lookAhead == atomo ){
+        infoAtomo = obterAtomo();
+        lookAhead = infoAtomo.atomo;
+    }
+    else{
+        printf("\n#%2d:Erro sintatico: esperado [%s] encontrado [%s]\n",infoAtomo.linha,strMensagem[atomo],strMensagem[lookahead]);
+        exit(1);
+    }
+}
+
+void program(){
+    consome(IDENTIFIER);
+    consome(KW_SEMICOLON);
+    block();
+    consome(KW_PERIOD);
 }
