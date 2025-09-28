5a8c418a7347651b11404be5a8a8a04517998734
Akuro <marcocamargoes@gmail.com>
2025-09-28 03:52:54 -0300

    Almost done

--- comp2/include/pasckenzie.h
+++ comp2/include/pasckenzie.h
@@ -36,11 +36,43 @@ typedef enum{
     EOS
 } TAtomo;
 
+const char *strMensagem[] = {
+    "ERRO",
+    "KW_PROGRAM",
+    "kW_SEMICOLON",
+    "KW_COMMA",
+    "KW_COLON",
+    "KW_OPEN_PARENTHESIS",
+    "KW_CLOSE_PARENTHESIS",
+    "KW_PERIOD",
+    "CONSTINT",
+    "CONSTCHAR",
+    "KW_VAR",
+    "KW_BEGIN",
+    "IDENTIFIER",
+    "TYPE",
+    "KW_READ",
+    "KW_WRITE",
+    "KW_IF",
+    "KW_ELSE",
+    "KW_THEN",
+    "KW_WHILE",
+    "KW_DO",
+    "RELATIONAL_OPERATOR",
+    "ADDING_OPERATOR",
+    "MULTIPLYING_OPERATOR",
+    "KW_NOT",
+    "KW_TOF",
+    "KW_END",
+    "ASSIGN",
+    "EOS"
+};
+
 typedef struct{
     TAtomo atomo;
     int linha;
     union{
-        int constInt;
+        float constInt;
         char id[16];
         char constChar;
     }atributo;
@@ -54,5 +86,25 @@ typedef struct{
 } keyword;
 // ------------------------------------------
 
-void obter_atomo();
+TInfoAtomo obter_atomo();
 void reconhece_id(TInfoAtomo* infoAtomo);
+void reconhece_num(TInfoAtomo *infoAtomo);
+void reconhece_char(TInfoAtomo *infoA);
+void reconhece_specialChars(TInfoAtomo *infoAtomo);
+void consome(TAtomo atomo);
+
+void program();
+void block();
+void variable_declaration_part();
+void variable_declaration();
+void statement_part();
+void statement();
+void factor();
+void term();
+void simple_expression();
+void expression();
+void assignment_statement();
+void read_statement();
+void write_statement();
+void if_statement();
+void while_statement();
--- comp2/pasckenzie.c
+++ comp2/pasckenzie.c
@@ -4,7 +4,7 @@
 char lexema[16];
 int nLinha;
 
-char* buffer = "\n\n\n (* This is a comment, \nI eat a bug *)\nprogram ex1;\n";
+char* buffer = "program teste;\nvar\n	num:integer;\nbegin\n	write(num);\nend.";
 
 TAtomo lookAhead;
 TInfoAtomo infoAtomo;
@@ -31,13 +31,15 @@ keyword keywords[] = {
     };
 
 
-void obter_atomo(TInfoAtomo* infoA){
-    infoA->atomo = ERRO;
+TInfoAtomo obter_atomo(){
+    printf("obter_atomo called\n");
+    TInfoAtomo infoA;
+    infoA.atomo = ERRO;
 
     for(;;){
         if(*buffer == '\0') {
-            infoA->atomo = EOS; 
-            return;
+            infoA.atomo = EOS; 
+            return infoA;
         }
         if(*buffer == ' ' || *buffer == '\t'){
             buffer++;
@@ -68,21 +70,32 @@ void obter_atomo(TInfoAtomo* infoA){
         break;
     }
 
-    
-    infoA->linha = nLinha;
+    infoA.linha = nLinha;
 
     if(islower(*buffer) || *buffer == '_'){
-        reconhece_id(infoA);
+        reconhece_id(&infoA);
     }
+    else if(isdigit(*buffer)){
+        reconhece_num(&infoA);
+    }
+    else if(*buffer == '\''){
+        reconhece_char(&infoA);
+    }
+    else{
+        reconhece_specialChars(&infoA);
+    }
+    return infoA;
+}
 
-
-
-
-
-
-
-
-    return;
+void consome(TAtomo atomo){
+    printf("consome called\n");
+    if(lookAhead == atomo){
+        infoAtomo = obter_atomo();
+        lookAhead = infoAtomo.atomo;
+    }else{
+        printf("\n#%2d:Erro sintatico\nEsperado[%s] - Entregue[%s]",infoAtomo.linha, strMensagem[atomo], strMensagem[lookAhead]);
+        exit(1);
+    }
 }
 
 void reconhece_id(TInfoAtomo* infoA){
@@ -105,25 +118,307 @@ void reconhece_id(TInfoAtomo* infoA){
         if(lexema[0] != '_'){ // palavras chave nunca começam com _, então se a string nova começar com _ ela pode só pular a verificação
             for(int i = 0; i < keywordSize; i++){
                 if(strcmp(lexema, keywords[i].keyword) == 0){
-                    printf("%i: %s\n", infoAtomo.linha, keywords[i].keyword);
+                    printf("%i: %s\n", infoA->linha, keywords[i].keyword);
                     infoA->atomo = keywords[i].value;
                     return;
                 }
             }
         }
-        printf("%i: identifier: %s\n", infoAtomo.linha, infoA->atributo.id);
+        printf("%i: %s: %s\n", infoA->linha, strMensagem[IDENTIFIER] ,infoA->atributo.id);
         infoA->atomo = IDENTIFIER;
         return;
 
 }
 
+void reconhece_num(TInfoAtomo *infoAtomo){
+    char *ini_lexema = buffer;
+
+    q1:
+        if(isdigit(*buffer)){
+            buffer++;
+            goto q1;
+        }
+        if(*buffer == '.'){
+            buffer++;
+            goto q2;
+        }
+        return;
+    q2:
+        if(isdigit(*buffer)){
+            buffer++;
+            goto q3;
+        }
+        return;
+    q3:
+        if(isdigit(*buffer)){
+            buffer++;
+            goto q3;
+        }
+        if(isalpha(*buffer)){
+            return;
+        }
+    
+        strncpy(lexema, ini_lexema, buffer-ini_lexema);
+        lexema[buffer-ini_lexema] = '\0';
+
+        infoAtomo->atributo.constInt = atof(lexema);
+
+        infoAtomo->atomo = CONSTINT;
+        printf("%i: %s: %f\n", infoAtomo->linha, strMensagem[CONSTINT], infoAtomo->atributo.constInt);
+
+        return;
+}
+
+void reconhece_char(TInfoAtomo *infoA){
+    buffer++;
+    if(*buffer == '\0'){infoA->atomo = EOS; return;}
+    char c = *buffer;
+    buffer++;
+    if(*buffer != '\''){
+        return;
+    }
+    buffer++;
+    infoA->atributo.constChar = c;
+    infoA->atomo = CONSTCHAR;
+    printf("%i: %s: '%c'\n", infoA->linha, strMensagem[CONSTCHAR], c);
+}
+
+void reconhece_specialChars(TInfoAtomo *infoAtomo){
+    struct {const char* key; TAtomo value;} values[] = {
+        {":=", ASSIGN},
+        {",", KW_COMMA},
+        {".", KW_PERIOD},
+        {":", KW_COLON},
+        {";", kW_SEMICOLON},
+        {"(", KW_OPEN_PARENTHESIS},
+        {")", KW_CLOSE_PARENTHESIS},
+        {"<>", RELATIONAL_OPERATOR},
+        {"<", RELATIONAL_OPERATOR},
+        {">", RELATIONAL_OPERATOR},
+        {"<=", RELATIONAL_OPERATOR},
+        {">=", RELATIONAL_OPERATOR},
+        {"=", RELATIONAL_OPERATOR},
+        {"or", RELATIONAL_OPERATOR},
+        {"and", RELATIONAL_OPERATOR},
+        {"+", ADDING_OPERATOR},
+        {"-", ADDING_OPERATOR},
+        {"*", MULTIPLYING_OPERATOR},
+        {"div", MULTIPLYING_OPERATOR}
+    };
+
+    if(buffer == NULL || *buffer == '\0'){
+        infoAtomo->atomo = EOS;
+        return;
+    }
+
+    size_t size = sizeof(values)/sizeof(values[0]);
+
+    for (size_t i = 0; i < size; ++i){
+        size_t len = strlen(values[i].key);
+        if(strncmp(values[i].key, buffer, len) == 0){
+            infoAtomo->atomo = values[i].value;
+            buffer += len;
+            printf("%i: %s\n", infoAtomo->linha, strMensagem[RELATIONAL_OPERATOR]);
+            return;
+        }
+    }
+    return;
+}
+
 int main(){
     printf("Analisando...\n");
     nLinha = 1;
-
-    obter_atomo(&infoAtomo);
-    obter_atomo(&infoAtomo);
+    infoAtomo = obter_atomo();
     lookAhead = infoAtomo.atomo;
+    program();
 
     printf("%i linhas analisadas, programa sintaticamente correto\n", nLinha);
+}
+
+void program(){
+    printf("program called\n");
+    consome(KW_PROGRAM);
+    printf("program consumed\n");
+    consome(IDENTIFIER);
+    consome(kW_SEMICOLON);
+    block();
+    consome(KW_PERIOD);
+}
+
+void block(){
+    printf("Block called\n");
+    variable_declaration_part();
+    statement_part();
+}
+
+void variable_declaration_part(){
+    printf("variable_declaration_part called\n");
+    if(lookAhead == KW_VAR){
+        consome(KW_VAR);
+        variable_declaration();
+        consome(kW_SEMICOLON);
+        while(lookAhead != KW_BEGIN){
+            variable_declaration();
+            consome(kW_SEMICOLON);
+        }
+    }
+}
+
+void variable_declaration(){
+    printf("variable_declaration called\n");
+    consome(IDENTIFIER);
+    while(lookAhead == KW_COMMA){
+        consome(KW_COMMA);
+        printf("\n $$$$$$$$$$$$$$ \n");
+        consome(IDENTIFIER);
+        printf("\n $$$$$$$$$$$$$$ \n");
+    }
+    consome(KW_COLON);
+    consome(TYPE);
+}
+
+void statement_part(){
+    printf("statement_part called\n");
+    consome(KW_BEGIN);
+    statement();
+    while(lookAhead == kW_SEMICOLON){
+        consome(kW_SEMICOLON);
+        statement();
+    }
+    consome(KW_END);
+}
+
+void statement(){
+    printf("statement called\n");
+    if(lookAhead == IDENTIFIER){
+        assignment_statement();
+        return;
+    }
+    if(lookAhead == KW_READ){
+        read_statement();
+        return;
+    }
+    if(lookAhead == KW_WRITE){
+        write_statement();
+        return;
+    }
+    if(lookAhead == KW_IF){
+        if_statement();
+        return;
+    }
+    if(lookAhead == KW_WHILE){
+        while_statement();
+        return;
+    }
+}
+
+void factor(){
+    printf("factor called\n");
+    if(lookAhead == IDENTIFIER){
+        consome(IDENTIFIER);
+        return;
+    }
+    if(lookAhead == CONSTCHAR){
+        consome(CONSTCHAR);
+        return;
+    }
+    if(lookAhead == CONSTINT){
+        consome(CONSTINT);
+        return;
+    }
+    if(lookAhead == KW_OPEN_PARENTHESIS){
+        consome(KW_OPEN_PARENTHESIS);
+        expression();
+        consome(KW_CLOSE_PARENTHESIS);
+        return;
+    }
+    if(lookAhead == KW_NOT){
+        consome(KW_NOT);
+        factor();
+        return;
+    }
+    if(lookAhead == KW_TOF){
+        consome(KW_TOF);
+        return;
+    }
+}
+
+void term(){
+    printf("term called\n");
+    factor();
+    while(lookAhead == MULTIPLYING_OPERATOR){
+        consome(MULTIPLYING_OPERATOR);
+        factor();
+    }
+}
+
+void simple_expression(){
+    printf("simple_expression called\n");
+    term();
+    while(lookAhead == ADDING_OPERATOR){
+        consome(ADDING_OPERATOR);
+        term();
+    }
+}
+
+void expression(){
+    printf("expression called\n");
+    simple_expression();
+    if(lookAhead == RELATIONAL_OPERATOR){
+        consome(RELATIONAL_OPERATOR);
+        simple_expression();
+    }
+}
+
+void assignment_statement(){
+    printf("assignment_statement called\n");
+    printf("SHOULD BE IDENTIFIER: %s\n", strMensagem[lookAhead]);
+    consome(IDENTIFIER);
+    printf("SHOULD BE ASSIGN: %s\n", strMensagem[lookAhead]);
+    consome(ASSIGN);
+    expression();
+}
+
+void read_statement(){
+    printf("read_statement called\n");
+    consome(KW_READ);
+    consome(KW_OPEN_PARENTHESIS);
+    consome(IDENTIFIER);
+    while(lookAhead == KW_COMMA){
+        consome(KW_COMMA);
+        consome(IDENTIFIER);
+    }
+    consome(KW_CLOSE_PARENTHESIS);
+}
+
+void write_statement(){
+    printf("write_statement called\n");
+    consome(KW_WRITE);
+    consome(KW_OPEN_PARENTHESIS);
+    consome(IDENTIFIER);
+    while(lookAhead == KW_COMMA){
+        consome(KW_COMMA);
+        consome(IDENTIFIER);
+    }
+    consome(KW_CLOSE_PARENTHESIS);
+}
+
+void if_statement(){
+    printf("if_statement called\n");
+    consome(KW_IF);
+    expression();
+    consome(KW_THEN);
+    statement();
+    if(lookAhead == KW_ELSE){
+        consome(KW_ELSE);
+        statement();
+    }
+}
+
+void while_statement(){
+    printf("while_statement called\n");
+    consome(KW_WHILE);
+    expression();
+    consome(KW_DO);
+    statement();
 }
